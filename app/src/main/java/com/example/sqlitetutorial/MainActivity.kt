3dd025e52dbdd64d79c863e958eeb2098caaa69a
ThanhTan <thanhtan27032001@gmail.com>
2024-02-24 20:21:43 +0700

    [v1.0.0] DAO - insert and select database

--- app/src/main/java/com/example/sqlitetutorial/MainActivity.kt
+++ app/src/main/java/com/example/sqlitetutorial/MainActivity.kt
@@ -9,6 +9,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        testWriteDatabase()
+        testReadDatabase()
+    }
 
+    private fun testWriteDatabase() {
+        val db = FeedReaderDbHelper(this)
+        db.insertNewFeed("Feed 1", "Sub title here")
+        db.insertNewFeed("Feed 2", "Sub title here")
+        db.insertNewFeed("Feed 3", "Sub title here")
+        db.insertNewFeed("Feed 4", "Sub title here")
+        db.insertNewFeed("Feed 5", "Sub title here")
+    }
+
+    private fun testReadDatabase() {
+        val db = FeedReaderDbHelper(this)
+        val cursor = db.getAllFeed()
+        while (cursor.moveToNext()) {
+            println(cursor.getString(1))
+        }
     }
 }
