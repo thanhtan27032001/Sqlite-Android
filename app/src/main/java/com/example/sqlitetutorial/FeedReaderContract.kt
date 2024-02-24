d17837228369bc99a9a9b96115afe2ffb42b2166
ThanhTan <thanhtan27032001@gmail.com>
2024-02-24 20:01:32 +0700

    [v1.0.0] DAO - FeedReaderDbHelper

---
+++ app/src/main/java/com/example/sqlitetutorial/FeedReaderContract.kt
@@ -0,0 +1,13 @@
+package com.example.sqlitetutorial
+
+import android.provider.BaseColumns
+
+object FeedReaderContract {
+    object FeedEntry: BaseColumns {
+        const val TABLE_NAME = "entry"
+        const val COLUMN_ID = "id"
+        const val COLUMN_TITLE = "title"
+        const val COLUMN_SUBTITLE = "subtitle"
+    }
+
+}
---
+++ app/src/main/java/com/example/sqlitetutorial/FeedReaderDbHelper.kt
@@ -0,0 +1,52 @@
+package com.example.sqlitetutorial
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.example.sqlitetutorial.FeedReaderContract.FeedEntry
+
+class FeedReaderDbHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        // If you change the database schema, you must increment the database version.
+        const val DATABASE_VERSION = 1
+        const val DATABASE_NAME = "FeedReader.db"
+
+        private const val SQL_CREATE_ENTRIES =
+            "CREATE TABLE ${FeedEntry.TABLE_NAME} (" +
+                    "${FeedEntry.COLUMN_ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "${FeedEntry.COLUMN_TITLE} TEXT," +
+                    "${FeedEntry.COLUMN_SUBTITLE} TEXT)"
+
+        private const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${FeedEntry.TABLE_NAME}"
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(SQL_CREATE_ENTRIES)
+    }
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DELETE_ENTRIES)
+        onCreate(db)
+    }
+
+    fun insertNewFeed(title: String, subtitle: String): Boolean {
+        val db = this.writableDatabase
+
+        val value = ContentValues()
+        value.put(FeedEntry.COLUMN_TITLE, title)
+        value.put(FeedEntry.COLUMN_SUBTITLE, subtitle)
+
+        val newRowId = db?.insert(FeedEntry.TABLE_NAME, null, value)
+
+        return newRowId?.toInt() != -1
+    }
+
+    fun getAllFeed(): Cursor {
+        val db = this.readableDatabase
+        return db.rawQuery("SELECT * FROM ${FeedEntry.TABLE_NAME}", null)
+    }
+}
